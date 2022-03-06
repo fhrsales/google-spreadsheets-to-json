c8f27a0945f3b23bd25ea49028d02709e8241013
fhrsales <fabio.sales@estadao.com>
2022-03-06 19:07:52 -0300

    updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // para rodar no servidor:
 // Copie os arquivos 'client.json', 'index.js', 'jobs.sh' e 'package.json' para a pasta do servidor
 // Rode o comando 'npm install'. Isso instalará todos os módulos necessários na pasta 'node_modules'
-// Digite crontab -e para adicionar o comando que executará o script 'index.js' todos os dias, à meia noite (0 0 * * *)
+// Digite 'crontab -e' para adicionar o comando que executará o script 'index.js' todos os dias, à meia noite (0 0 * * *)
 // Digite 'i'
 // 0 0 * * * /bin/sh /var/www/html/atelie/rabiscos/google-spreadsheets-to-json/jobs.sh
 // Digite 'esc'
